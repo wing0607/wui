ac0b5f3f6dca994399050dec9c8396e058994ec1
nbaisino\tongyingting <731771480@qq.com>
2018-05-30 17:31:29 +0800

    目录

---
+++ wing/json/menu.js
@@ -0,0 +1,14 @@
+{
+    "code":0,
+    "msg":"",
+    "data":[{
+      "name":"component",
+      "title": "组件",
+      "icon": "",
+      "list":[{
+          "name":"table",
+          "title": "表格",
+      }]
+
+  }]
+}
